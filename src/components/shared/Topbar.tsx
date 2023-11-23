e1350f5270c9fcd2cc24f5fddc6aa4e46926d2ce
Nataliya Kim <magistr1982@gmail.com>
2023-11-23 09:36:33 +0200

    Create Topbar component

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom';
+import { Button } from '../ui/button';
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          <div className="flex flex-row gap-4">
+            <img
+              src="/assets/images/logo-form.png"
+              alt="logo"
+              width={48}
+              height={48}
+            />
+            <p className="h1-medium font-fingerPaint">UniVibe</p>
+          </div>
+        </Link>
+        <div className="flex gap-4">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            // onClick={signOut}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Topbar
+export default Topbar;
