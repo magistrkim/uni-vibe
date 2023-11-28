04c2cc5b6eb3ea25692a85e977726288a1e8fd3d
Nataliya Kim <magistr1982@gmail.com>
2023-11-28 15:15:15 +0200

    Add useCreatePost

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -32,6 +32,7 @@ export const useSignOutAccount = () => {
 };
 export const useCreatePost = () => {
   const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (post: INewPost) => createPost(post),
     onSuccess: () => {
