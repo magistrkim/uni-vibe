28f0d10b107c32e5f41e94590f700db436b0db4a
Nataliya Kim <magistr1982@gmail.com>
2023-12-17 17:10:55 +0200

    Add GridPostList UI

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,20 +1,44 @@
 import { Models } from 'appwrite';
 import { Link } from 'react-router-dom';
 import { useUserContext } from '@/context/AuthContext';
+import PostStats from './PostStats';
 
 type GridPostListProps = {
   posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
 };
 
-const GridPostList = ({ posts }: GridPostListProps) => {
+const GridPostList = ({
+  posts,
+  showUser = true,
+  showStats = true,
+}: GridPostListProps) => {
   const { user } = useUserContext();
   return (
     <ul className="grid-container">
       {posts.map(post => (
         <li key={post.$id} className="relative min-w-80 h-80">
-          <Link to={`/posts/${post.$id}`} className='grid-post_link'> 
-          <img src={post.imageUrl} alt="post image" className='w-full h-full object-cover'/>
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
+            <img
+              src={post.imageUrl}
+              alt="post image"
+              className="w-full h-full object-cover"
+            />
           </Link>
+          <div className="grid-post_user">
+            {showUser && (
+              <div className="flex gap-2 justify-start items-center flex-1">
+                <img
+                  src={post.creator.imageUrl}
+                  alt="user image"
+                  className="h-8 w-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
