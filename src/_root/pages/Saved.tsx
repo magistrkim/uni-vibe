4f2cd4077726f77ce160e9f6c34f954b387a33f4
Nataliya Kim <magistr1982@gmail.com>
2023-12-25 10:55:01 +0200

    Create Saved page UI

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,9 +1,37 @@
-
+import { Models } from 'appwrite';
+import { useGetCurrentUser } from '@/lib/react-query/queriesAndMutations';
+import GridPostList from '@/components/shared/GridPostList';
+import Loader from '@/components/shared/Loader';
 
 const Saved = () => {
+  const { data: currentUser } = useGetCurrentUser();
+  const savePosts = currentUser?.save
+    .map((savePost: Models.Document) => ({
+      ...savePost.post,
+      creator: {
+        imageUrl: currentUser.imageUrl,
+      },
+    }))
+    .reverse();
   return (
-    <div>Saved</div>
-  )
-}
+    <div className="saved-container">
+      <div className="flex w-full gap-2 max-w-5xl">
+        <img src="/assets/icons/save.svg" alt="save" width={36} height={36} />
+        <h2 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h2>
+      </div>
+      {!currentUser ? (
+        <Loader />
+      ) : (
+        <ul className="w-full flex justify-center max-w-5xl gap-9">
+          {savePosts.length === 0 ? (
+            <p className="text-light-4">No available posts</p>
+          ) : (
+            <GridPostList posts={savePosts} showStats={false} />
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default Saved
+export default Saved;
