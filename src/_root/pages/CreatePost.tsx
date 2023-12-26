782762f861a329813d3ebe88d408a7287a606f2a
Nataliya Kim <magistr1982@gmail.com>
2023-12-26 16:50:00 +0200

    Add sidebar to CreatePost

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,6 +1,11 @@
 import PostForm from '@/components/forms/PostForm';
+import UserCard from '@/components/shared/UserCard';
+import { useUserContext } from '@/context/AuthContext';
+// import GridPostList from '@/components/shared/GridPostList';
 
 const CreatePost = () => {
+  const { user } = useUserContext();
+
   return (
     <div className="flex flex-1">
       <div className="common-container">
@@ -14,6 +19,13 @@ const CreatePost = () => {
           <h2 className="h3-bold md:h2-bold w-full text-left">Create post</h2>
         </div>
         <PostForm action="Create" />
+      </div>
+      <div className="home-creators">
+        <UserCard user={user} />
+        <h3 className="body-bold text-light-1">Top posts by you</h3>
+        {/* <div className="flex flex-col">
+          <GridPostList posts={currentUser.posts} showUser={false} customContainerClass="update-col"/>
+        </div> */}
       </div>
     </div>
   );
